a1337f5119a30f8a9f7a8d78b5ae88fd4634d2e9
Praise Tompane <praisetompane@icloud.com>
2025-02-27 10:23:46 +0200

    docs: clean up

--- create_project.sh
+++ create_project.sh
@@ -1,10 +1,8 @@
 #!/bin/zsh
-#
-#    quick utility to create Django project.
-#    accepts project name as parameter
-#    Example:
-#    $ ./create_project.sh my_project
-#
+# quick utility to create Django project.
+# accepts project name as parameter
+# Example:
+#  $ ./create_project.sh my_project
 
 project=$1
 
@@ -12,7 +10,7 @@ echo "creating project"
     django-admin startproject $project
 echo "done"
 
-cp run_project.sh ./$project
+cp start_system_development.sh ./$project
 cp create_app.sh ./$project
 cp .gitignore ./$project
 touch ./$project/README.md
